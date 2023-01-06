361c221e0fdf06e10caac21d89072824c8a2c52f
MilenaSuzumura <milena.suzumura11@gmail.com>
2023-01-06 14:53:39 -0300

    Finalmente Terminei PVP!!!

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,17 +1,22 @@
-/* import Battle from './Battle';
 import Fighter from '../Fighter';
+import Battle from './Battle';
 
 export default class PVP extends Battle {
   private character1: Fighter;
   private character2: Fighter;
 
   constructor(character1: Fighter, character2: Fighter) {
-    super.fight(character1);
+    super(character1);
     this.character1 = character1;
     this.character2 = character2;
   }
 
   fight() {
-    
+    if (this.character1.lifePoints > 0 && this.character2.lifePoints > 0) {
+      this.character1.attack(this.character2);
+      this.character2.attack(this.character1);
+    }
+
+    return super.fight();
   }
-} */
+}
